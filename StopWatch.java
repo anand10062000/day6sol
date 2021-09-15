4423e2db7025a40873f3f74516fbf61be0ff2f24
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-15 17:45:54 +0530

    code for stopwatch

---
+++ StopWatch.java
@@ -0,0 +1,23 @@
+  public double StopWatch() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        Stopwatch timer1 = new Stopwatch();
+        double sum1 = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum1 += Math.sqrt(i);
+        }
+        double time1 = timer1.elapsedTime();
+        StdOut.printf("%e (%.2f seconds)\n", sum1, time1);
+
+        
+        Stopwatch timer2 = new Stopwatch();
+        double sum2 = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum2 += Math.pow(i, 0.5);
+        }
+        double time2 = timer2.elapsedTime();
+        StdOut.printf("%e (%.2f seconds)\n", sum2, time2);
+    }
