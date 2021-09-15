956ab11eea2930b7e89b681ae0d7ff464df0fe9a
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-15 18:02:37 +0530

    code for swapping of nibble num

---
+++ SwapNibble.java
@@ -0,0 +1,15 @@
+class SwapNibble {
+     
+static int swapNibbles(int x)
+{
+    return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+}
+ 
+// Driver code
+public static void main(String arg[])
+{
+    int x = 100;
+    System.out.print(swapNibbles(x));
+    int sqrtvalue=x*x;
+}
+}
